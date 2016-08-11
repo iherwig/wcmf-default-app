9a4bcdbe68b3e7f9f5450fa817eab813b3765d13
iherwig <ingo@wemove.com>
2016-08-12 01:13:12 +0200

    progress bar on buttons (work in progress)

--- app/public/js/ui/_include/widget/Button.js
+++ app/public/js/ui/_include/widget/Button.js
@@ -1,13 +1,36 @@
 define([
     "dojo/_base/declare",
+    "dojo/dom-style",
+    "dojo/dom-construct",
     "dijit/form/Button"
 ], function (
     declare,
+    domStyle,
+    domConstruct,
     Button
 ) {
     return declare([Button], {
 
         initialLabel: "",
+        progress: 0,
+
+        postCreate: function () {
+            this.inherited(arguments);
+
+            domStyle.set(this.domNode, 'position', 'relative');
+            this.progress = domConstruct.create('span', {
+                style: {
+                    background: '#ddd',
+                    position: 'absolute',
+                    left: 0,
+                    width: this.progress,
+                    height: '100%',
+                    opacity: 0.5,
+                    borderRadius: '4px 0 0 4px'
+                }
+            });
+            domConstruct.place(this.progress, this.domNode, 'first');
+        },
 
         setProcessing: function() {
             this.initialLabel = this.get("label");
