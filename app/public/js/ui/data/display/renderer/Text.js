8bae2bfa4045faddb8dfcc2c67ccf314f81fa61a
iherwig <ingo@wemove.com>
2018-10-04 18:06:15 +0200

    Remove tags from text display values

--- app/public/js/ui/data/display/renderer/Text.js
+++ app/public/js/ui/data/display/renderer/Text.js
@@ -5,6 +5,10 @@ function(
     ControlFactory
 ) {
     return function(value, attribute) {
+        if (typeof value == 'string') {
+            // remove tags
+            value = value.replace(/<[^>]*>/g, '');
+        }
         return ControlFactory.translateValue(attribute.inputType, attribute.displayType, value);
     };
 });
