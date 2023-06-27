c6caf10e9d7381c4337f1e0174175b1d2563ce48
iherwig <ingo@wemove.com>
2023-06-27 16:57:36 +0200

    Restructure directories

---
+++ app/src/components/page/BaseFooter.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-row class="items-center">
+    <el-col :span="12">
+      <div>
+        <small>{{ appProperties.appTitle }} <em>{{ appProperties.version }}</em> created with <a href="https://wcmf.wemove.com" target="_blank">wCMF</a></small>
+      </div>
+    </el-col>
+    <el-col :span="12">
+      <div class="flex gap-2 justify-end">
+        <LocaleChanger :onlyIfMultiple="true" />
+        <el-button circle @click="toggleDark()">
+          <el-icon v-if="isDark" class="el-icon"><Moon /></el-icon>
+          <el-icon v-else class="el-icon"><Sunny /></el-icon>
+        </el-button>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script lang="ts" setup>
+import { isDark, toggleDark, useAppProperties } from '~/composables'
+import { Moon, Sunny } from '@element-plus/icons-vue'
+
+const appProperties = useAppProperties()
+</script>
+
+<style scoped>
+.ep-row {
+  height: 100%;
+}
+</style>
